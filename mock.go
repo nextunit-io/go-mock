1fd4c363446a39943b8815170b71e1721b7a3ff8
Nils B. <zero+github@nextunit.io>
2024-07-30 16:35:11 +0200

    Rename structs, not necessarily AWS involved

--- mock.go
+++ mock.go
@@ -1,24 +1,24 @@
 package gomock
 
-type awsToolsMockReturnValue[U any] struct {
+type toolsMockReturnValue[U any] struct {
 	Value *U
 	Error error
 }
 
-type AwsToolMock[T any, U any] struct {
+type ToolMock[T any, U any] struct {
 	generalError error
 	inputParams  []T
-	returnValues []awsToolsMockReturnValue[U]
+	returnValues []toolsMockReturnValue[U]
 }
 
-func GetMock[T any, U any](generalError error) *AwsToolMock[T, U] {
-	return &AwsToolMock[T, U]{
+func GetMock[T any, U any](generalError error) *ToolMock[T, U] {
+	return &ToolMock[T, U]{
 		generalError: generalError,
 		inputParams:  []T{},
 	}
 }
 
-func (mock *AwsToolMock[T, U]) AddInput(i T) {
+func (mock *ToolMock[T, U]) AddInput(i T) {
 	if mock.inputParams == nil {
 		mock.inputParams = []T{}
 	}
@@ -26,35 +26,35 @@ func (mock *AwsToolMock[T, U]) AddInput(i T) {
 	mock.inputParams = append(mock.inputParams, i)
 }
 
-func (mock AwsToolMock[T, U]) GetInputs() []T {
+func (mock ToolMock[T, U]) GetInputs() []T {
 	return mock.inputParams
 }
 
-func (mock AwsToolMock[T, U]) HasBeenCalled() int {
+func (mock ToolMock[T, U]) HasBeenCalled() int {
 	return len(mock.inputParams)
 }
 
-func (mock AwsToolMock[T, U]) GetInput(position int) T {
+func (mock ToolMock[T, U]) GetInput(position int) T {
 	return mock.inputParams[position]
 }
 
-func (mock *AwsToolMock[T, U]) AddReturnValue(value *U) {
+func (mock *ToolMock[T, U]) AddReturnValue(value *U) {
 	if mock.returnValues == nil {
-		mock.returnValues = []awsToolsMockReturnValue[U]{}
+		mock.returnValues = []toolsMockReturnValue[U]{}
 	}
 
-	mock.returnValues = append(mock.returnValues, awsToolsMockReturnValue[U]{
+	mock.returnValues = append(mock.returnValues, toolsMockReturnValue[U]{
 		Value: value,
 	})
 }
 
-func (mock *AwsToolMock[T, U]) prepareReturnArray(position int) {
+func (mock *ToolMock[T, U]) prepareReturnArray(position int) {
 	if mock.returnValues == nil {
-		mock.returnValues = []awsToolsMockReturnValue[U]{}
+		mock.returnValues = []toolsMockReturnValue[U]{}
 	}
 
 	if position >= len(mock.returnValues) {
-		r := make([]awsToolsMockReturnValue[U], position+1)
+		r := make([]toolsMockReturnValue[U], position+1)
 		for i := 0; i < position; i++ {
 			if i < len(mock.returnValues) {
 				r[i] = mock.returnValues[i]
@@ -65,45 +65,45 @@ func (mock *AwsToolMock[T, U]) prepareReturnArray(position int) {
 	}
 }
 
-func (mock *AwsToolMock[T, U]) SetReturnValue(position int, value *U) {
+func (mock *ToolMock[T, U]) SetReturnValue(position int, value *U) {
 	mock.prepareReturnArray(position)
 
 	if position < len(mock.returnValues) {
-		mock.returnValues[position] = awsToolsMockReturnValue[U]{
+		mock.returnValues[position] = toolsMockReturnValue[U]{
 			Value: value,
 		}
 	} else {
-		mock.returnValues[position] = awsToolsMockReturnValue[U]{
+		mock.returnValues[position] = toolsMockReturnValue[U]{
 			Value: value,
 		}
 	}
 }
 
-func (mock *AwsToolMock[T, U]) AddError(err error) {
+func (mock *ToolMock[T, U]) AddError(err error) {
 	if mock.returnValues == nil {
-		mock.returnValues = []awsToolsMockReturnValue[U]{}
+		mock.returnValues = []toolsMockReturnValue[U]{}
 	}
 
-	mock.returnValues = append(mock.returnValues, awsToolsMockReturnValue[U]{
+	mock.returnValues = append(mock.returnValues, toolsMockReturnValue[U]{
 		Error: err,
 	})
 }
 
-func (mock *AwsToolMock[T, U]) SetError(position int, err error) {
+func (mock *ToolMock[T, U]) SetError(position int, err error) {
 	mock.prepareReturnArray(position)
 
 	if position < len(mock.returnValues) {
-		mock.returnValues[position] = awsToolsMockReturnValue[U]{
+		mock.returnValues[position] = toolsMockReturnValue[U]{
 			Error: err,
 		}
 	} else {
-		mock.returnValues[position] = awsToolsMockReturnValue[U]{
+		mock.returnValues[position] = toolsMockReturnValue[U]{
 			Error: err,
 		}
 	}
 }
 
-func (mock *AwsToolMock[T, U]) GetNextResult() (*U, error) {
+func (mock *ToolMock[T, U]) GetNextResult() (*U, error) {
 	if mock.returnValues == nil {
 		return nil, mock.generalError
 	}
@@ -112,7 +112,7 @@ func (mock *AwsToolMock[T, U]) GetNextResult() (*U, error) {
 		return nil, mock.generalError
 	}
 
-	var x awsToolsMockReturnValue[U]
+	var x toolsMockReturnValue[U]
 	x, mock.returnValues = mock.returnValues[0], mock.returnValues[1:]
 
 	if x.Error != nil {
@@ -126,7 +126,7 @@ func (mock *AwsToolMock[T, U]) GetNextResult() (*U, error) {
 	return x.Value, nil
 }
 
-func (mock *AwsToolMock[T, U]) Reset() {
+func (mock *ToolMock[T, U]) Reset() {
 	mock.returnValues = nil
 	mock.inputParams = []T{}
 }
